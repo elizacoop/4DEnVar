478a40b6645f5b1eea623132ccc35783eca1101f
Elizabeth Cooper <elicoo@ceh.ac.uk>
2026-07-16 10:29:55 +0100

    Changes to run provided linear example test on my system

--- fourDEnVar.py
+++ fourDEnVar.py
@@ -6,15 +6,15 @@ importlib.reload(fourDEnVar_engine)
 
 if __name__=="__main__":
 
-    Xb=np.genfromtxt("Xb.dat")
-    hX=np.genfromtxt("hX.dat")
-    y=np.genfromtxt("y.dat")
-    R=np.genfromtxt("R.dat")
-    hx_bar=np.genfromtxt("Hxbar.dat")
+    Xb=np.genfromtxt("linear_tests/Xb.dat")
+    hX=np.genfromtxt("linear_tests/HX.dat")
+    y=np.genfromtxt("linear_tests/y.dat")
+    R=np.genfromtxt("linear_tests/R.dat")
+    hx_bar=np.genfromtxt("linear_tests/Hxbar.dat")
     
     x=fourDEnVar_engine.fourDEnVar_engine(Xb, hX, y, R, hx_bar)
     xa=x.xa
-    Xa=x.X_a
+    Xa=x.Xa
 
     print(xa)
     print(Xa)
